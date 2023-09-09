de57288f2529645c4ecd2eb83d035d8962974e3b
Nicholas <kathnick01@gmail.com>
2023-09-09 16:22:07 +0800

    add custom quiz page with routing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor(private navCtrl: NavController) {
-
-  }
+  constructor(private navCtrl: NavController) {}
 
   navigateToAnotherPage() {
     this.navCtrl.navigateForward('/tabs/tab2');
   }
 
+  navigateToQuizPage() {
+    this.navCtrl.navigateForward('/tabs/tab4');
+  }
 }
